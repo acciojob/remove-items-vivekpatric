36c8e36c454f529a7ca9d25e0e8c9669ad404201
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 23:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let button=document.getElementsByTagName("input")[0];
+button.addEventListener('click',removee);
+
+function removee() {
+	const option=document.getElementById("colorSelect").value;
+	document.getElementById(option).remove();
+	//return false;
+}
+// const submit = document.getElementsByTagName("input")[0];
+// submit.addEventListener('click', ()=>{
+// 	const value = document.getElementById("colorSelect").value;
+// 	document.getElementById(value).remove();
+// })
